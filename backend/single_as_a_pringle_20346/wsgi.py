e82c6b96b3f4430543bd381ca7468297de183602
Crowdbotics <team@crowdbotics.com>
2020-09-16 22:14:56 +0000

    Update models

--- backend/single_as_a_pringle_20346/wsgi.py
+++ backend/single_as_a_pringle_20346/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'single_as_a_pringle_20346.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "single_as_a_pringle_20346.settings")
 
 application = get_wsgi_application()
